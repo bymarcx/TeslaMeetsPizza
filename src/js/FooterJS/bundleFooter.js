a28ab3633a33dc15784915199b03921abdfe34d4
Marc Eberhard <marcf.eberhard@gmail.com>
2020-12-27 18:25:28 +0100

    final touches

--- src/js/FooterJS/bundleFooter.js
+++ src/js/FooterJS/bundleFooter.js
@@ -7,28 +7,9 @@ import 'bootstrap';
 
 import fullpage from 'fullpage.js';
 
-
 require("./Custom/custom");
-//require("./Custom/init-fp");
-
-console.log("TESTING");
-
-// refresh page on browser resize
-// initalizing fullpage new on every resize
-// FULLPAGE: "mimimimi" 
-// $(window).bind('resize', function(e)
-// {
-//   console.log('window resized..');
-//   this.location.reload(false); /* false to get page from cache */
-// });
-
-
-
 
 $(document).ready(function() {
-
-  // It's only fullpage on desktop!
-  //if($(window).width() > 992) {
 
     // initalizing fullpage.js / Letz fetz!
     var fullPageInstance = new fullpage('#fullpage', {
@@ -46,118 +27,12 @@ $(document).ready(function() {
       navigationTooltips: ['Model S', 'Geschmack', 'Performance', 'Nachhaltigkeit', 'Wichtige Daten', 'Bestellen'],
       responsiveWidth: 992,
 
-      afterLoad: function( origin, destination, direction){
-      var loadedSection = this;
-
-        // using destination index to change logo color.
-        if(destination.index == 0){
-          //document.getElementById("textlogo").style.fill="white";
-        }
-        if(destination.index == 1){
-          //document.getElementById("textlogo").style.fill="black";
-          $(".fade").fadeIn();
-        }
-        if(destination.index == 2){
-          //document.getElementById("textlogo").style.fill="white";
-          
-        }
-
-      } 
-
     });
 
     //methods
     fullpage_api.setAllowScrolling(true);
 
- // }
-
   });
-
-
-
-
-
-
-$(document).ready(function () {
-
-    $('#los').click(function(){
-
-        if(!$("#collapseExample").hasClass('show')){
-
-            // document.getElementById("stage").style.display = "none";
-            // document.getElementById("sec2").style.display = "none";
-            // document.getElementById("sec3").style.display = "none";
-            // document.getElementById("sec4").style.display = "none";
-
-            document.querySelector('#sec1').classList.add('fp-normal-scroll');
-            document.querySelector('#sec1').classList.add('fp-scrollable');
-            //fullpage_api.setAllowScrolling(false);
-
-            fullpage_api.reBuild();
-
-            $('#sec1').animate({
-              scrollTop: $("#collapseSec1").offset().top
-            }, 1000);
-
-            // setTimeout(function() {
-            //     $.fn.fullpage.setAllowScrolling(false);
-            //     $.fn.fullpage.reBuild();
-            //     console.log("set scrolling false;")
-            // }, 100);
-            
-            
-
-
-            // if (document.querySelector('#fp-nav') !== null) {
-            //     document.querySelector('#fp-nav').classList.add('big');
-            // }
-
-            // // setTimeout(function() {
-            // //     scrollToElement(collapseExample);
-                
-            // // }, 300);
-            
-
-        }
-        else {
-
-
-            document.querySelector('#sec1').classList.remove('fp-normal-scroll');
-            document.querySelector('#sec1').classList.remove('fp-scrollable');
-
-            //fullpage_api.setAllowScrolling(true);
-            
-            fullpage_api.reBuild();
-
-
-            
-            //$.fn.fullpage.moveTo(1);
-
-            // setTimeout(function() {
-            //   $.fn.fullpage.setAllowScrolling(true);
-            //   $.fn.fullpage.reBuild();
-              
-            //   console.log("set scrolling true;")
-            // }, 300);
-
-            // $.fn.fullpage.moveTo(3);
-
-            // document.getElementById("stage").style.display = "table";
-            // document.getElementById("sec2").style.display = "table";
-            // document.getElementById("sec3").style.display = "table";
-            // document.getElementById("sec4").style.display = "table";
-
-            // if (document.querySelector('#fp-nav') !== null) {
-            //     document.querySelector('#fp-nav').classList.remove('big');
-            // }
-            
-        }
-
-    });
-    
-});
-
-
 
 
 // MOBILE NAVBAR!!!
@@ -194,7 +69,7 @@ window.onscroll = function() {
 
 
 // COUNT UP
-
+// Count up number on stage
 setTimeout(function() {
   $('.counter').each(function() {
     var $this = $(this),
@@ -220,5 +95,3 @@ setTimeout(function() {
   });
 }, 1200);
 
-// ACTIVATE BOOTSTRAP TAB NAV
-// $('#myTab').tabCollapse();

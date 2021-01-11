14f66723db9931b1805c714ae8ba0efab8ad17da
Marc Eberhard <marcf.eberhard@gmail.com>
2021-01-11 17:36:05 +0100

    latest fixes

--- src/js/FooterJS/bundleFooter.js
+++ src/js/FooterJS/bundleFooter.js
@@ -8,57 +8,57 @@ import fullpage from 'fullpage.js';
 
 require("./Custom/custom");
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // initalizing fullpage.js :: Letz fetz!
-    var fullPageInstance = new fullpage('#fullpage', {
+  // initalizing fullpage.js :: Letz fetz!
+  var fullPageInstance = new fullpage('#fullpage', {
 
-      // options here
-      scrollBar: true,
-      autoScrolling:true,
-      scrollHorizontally: false,
-      css3: true,
-      lockAnchors: true,
-      navigation: true,
-      navigationPosition: 'left',
-      navigationTooltips: ['Model S', 'Geschmack', 'Performance', 'Nachhaltigkeit', 'Wichtige Daten', 'Bestellen'],
-      responsiveWidth: 992,
+    // options here
+    scrollBar: true,
+    autoScrolling: true,
+    scrollHorizontally: false,
+    css3: true,
+    lockAnchors: true,
+    navigation: true,
+    navigationPosition: 'left',
+    navigationTooltips: ['Model S', 'Geschmack', 'Performance', 'Nachhaltigkeit', 'Wichtige Daten', 'Bestellen'],
+    responsiveWidth: 992,
 
-      afterRender: function(){
+    afterRender: function () {
 
-        // make tooltip clickable
-        Array.prototype.forEach.call(document.querySelectorAll('#fp-nav ul li .fp-tooltip'), function(t, i){
-          t.addEventListener('click', function(e) {
-            event.target.parentElement.querySelector('a').dispatchEvent(new MouseEvent('click', {
-              bubbles: true,
-              cancelable: true,
-              view: window
-            }))
-          });
+      // make tooltip clickable
+      Array.prototype.forEach.call(document.querySelectorAll('#fp-nav ul li .fp-tooltip'), function (t, i) {
+        t.addEventListener('click', function (e) {
+          event.target.parentElement.querySelector('a').dispatchEvent(new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true,
+            view: window
+          }))
         });
-  
-      },
+      });
 
-    });
-
-    //methods
-    fullpage_api.setAllowScrolling(true);
+    },
 
   });
+
+  //methods
+  fullpage_api.setAllowScrolling(true);
+
+});
 
 
 // MOBILE NAVBAR
 // When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
 
-  if( currentScrollPos <= (5) ) {
+  if (currentScrollPos <= (5)) {
     document.getElementById("header").style.top = "0";
     $("#header").removeClass("fixed");
-    
+
   }
-  else if (currentScrollPos <= (window.innerHeight - 300 )) {    
+  else if (currentScrollPos <= (window.innerHeight - 300)) {
     document.getElementById("header").style.top = "-150px";
 
   }
@@ -79,27 +79,27 @@ window.onscroll = function() {
 
 // COUNT UP
 // Count up number on stage
-setTimeout(function() {
-  $('.counter').each(function() {
+setTimeout(function () {
+  $('.counter').each(function () {
     var $this = $(this),
-        countTo = $this.attr('data-count');
-    
-    $({ countNum: $this.text()}).animate({
+      countTo = $this.attr('data-count');
+
+    $({ countNum: $this.text() }).animate({
       countNum: countTo
     },
 
-    {
+      {
 
-      duration: 1500,
-      easing:'swing',
-      step: function() {
-        $this.text(Math.floor(this.countNum));
-      },
-      complete: function() {
-        $this.text(this.countNum);
-      }
+        duration: 1500,
+        easing: 'swing',
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum);
+        }
 
-    });  
+      });
   });
 }, 1200);
 
